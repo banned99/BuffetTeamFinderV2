d33e3bb3a9edbe1716a08eb086f3e94b7d065cb7
icespw <icesupawan@hotmail.com>
2016-10-27 06:27:02 +0700

    tum rai mai ru 55555

--- app/src/main/java/com/buffet/activities/ChooseBranchActivity.java
+++ app/src/main/java/com/buffet/activities/ChooseBranchActivity.java
@@ -92,6 +92,7 @@ public class ChooseBranchActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 drawerLayout.closeDrawers();
+
                 Intent navIntent = new Intent(v.getContext().getApplicationContext(), ViewProfileActivity.class);
                 startActivity(navIntent);
             }
@@ -125,7 +126,7 @@ public class ChooseBranchActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
