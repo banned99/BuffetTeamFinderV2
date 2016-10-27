3e20d3d98fa4e26cc2b7fb0322bc5404ac70b3ba
icespw <icesupawan@hotmail.com>
2016-10-27 14:04:52 +0700

    hmong ang

--- app/src/main/java/com/buffet/dialogs/CreateDealDialog.java
+++ app/src/main/java/com/buffet/dialogs/CreateDealDialog.java
@@ -55,8 +55,6 @@ public class CreateDealDialog extends DialogFragment{
             options.add(""+i);
         }
 
-        setCancelable(false);
-
         view = inflater.inflate(R.layout.create_deal_dialog, null);
 
         DATE = (TextView) view.findViewById(R.id.date_choose);
